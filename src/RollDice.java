34f5ae6f1260b5f7e15fdfa5f4fbd402edf09bce
grahamcwycyshyn <grahamcwycyshyn@gmail.com>
2019-07-23 16:07:11 -0400

    initial commit

--- src/RollDice.java
+++ src/RollDice.java
@@ -1,73 +1,152 @@
 
 public class RollDice {
-	
-	
+
 	public static int yahtzee(int[] dice, String category) {
 		int sum = 0;
 
-		if(category.equalsIgnoreCase("one")) {
-			for(int die : dice) {
+		if (category.equalsIgnoreCase("one")) {
+			for (int die : dice) {
 				if (die == 1) {
 					sum += die;
 				}
-			} return sum;
+			}
+			return sum;
 		}
-		
-		if(category.equalsIgnoreCase("two")) {
-			for(int die : dice) {
+
+		if (category.equalsIgnoreCase("two")) {
+			for (int die : dice) {
 				if (die == 2) {
 					sum += die;
 				}
-			} return sum;
+			}
+			return sum;
 		}
-		
-		if(category.equalsIgnoreCase("three")) {
-			for(int die : dice) {
+
+		if (category.equalsIgnoreCase("three")) {
+			for (int die : dice) {
 				if (die == 3) {
 					sum += die;
 				}
-			} return sum;
+			}
+			return sum;
 		}
-		
-		if(category.equalsIgnoreCase("four")) {
-			for(int die : dice) {
+
+		if (category.equalsIgnoreCase("four")) {
+			for (int die : dice) {
 				if (die == 4) {
 					sum += die;
 				}
-			} return sum;
+			}
+			return sum;
 		}
-		
-		if(category.equalsIgnoreCase("five")) {
-			for(int die : dice) {
+
+		if (category.equalsIgnoreCase("five")) {
+			for (int die : dice) {
 				if (die == 5) {
 					sum += die;
 				}
-			} return sum;
+			}
+			return sum;
 		}
-		
-		if(category.equalsIgnoreCase("six")) {
-			for(int die : dice) {
+
+		if (category.equalsIgnoreCase("six")) {
+			for (int die : dice) {
 				if (die == 6) {
 					sum += die;
 				}
-			} return sum;
+			}
+			return sum;
 		}
-		
-		if(category.equalsIgnoreCase("yahtzee")) {
-			for(int i = 0; i < dice.length; i++) {
-				if(dice[0] == dice[1] && dice[1] == dice[2] && dice[2] == dice[3] && dice[3] == dice[4]) {
+
+		if (category.equalsIgnoreCase("yahtzee")) {
+			for (int i = 0; i < dice.length; i++) {
+				if (dice[0] == dice[1] && dice[1] == dice[2] && dice[2] == dice[3] && dice[3] == dice[4]) {
 					return 50;
 				} else {
 					return 0;
 				}
 			}
 		}
-		
 
+		if (category.equalsIgnoreCase("pair")) {
+			int x = 0;
+			for (int i = 1; i <= 6; i++) {
+				if (diceCount(dice, i) >= 2) {
+					x = i;
+				}
+			}
+			return 2 * x;
+		}
+
+		if (category.equalsIgnoreCase("pair")) {
+			int x = 0;
+			for (int i = 1; i <= 6; i++) {
+				if (diceCount(dice, i) >= 2) {
+					x = i;
+				}
+			}
+			return 2 * x;
+		}
+
+		if (category.equalsIgnoreCase("two pair")) {
+			int x = 0;
+			int y = 0;
+			for (int i = 1; i <= 6; i++) {
+				for (int j = 1; j <= 6; j++) {
+					if (diceCount(dice, i) >= 2 && diceCount(dice, j) >= 2 && i != j) {
+						x = i;
+						y = j;
+					}
+				}
+			}
+			return 2 * (x + y);
+		}
+
+		if (category.equalsIgnoreCase("three of a kind")) {
+			int x = 0;
+			for (int i = 1; i <= 6; i++) {
+				if (diceCount(dice, i) >= 3) {
+					x = i;
+				}
+			}
+			return 3 * x;
+		}
+		
+		if (category.equalsIgnoreCase("four of a kind")) {
+			int x = 0;
+			for (int i = 1; i <= 6; i++) {
+				if (diceCount(dice, i) >= 4) {
+					x = i;
+				}
+			}
+			return 4 * x;
+		}
+		
+		if (category.equalsIgnoreCase("small straight")) {
+			int x = 0;
+			for (int i = 1; i <= 5; i++) {
+				if (diceCount(dice, i) == 1) {
+					x++;
+				}
+			}
+			if(x == 5) {
+				return 15;
+			} else {
+				return 0;
+			}
+		}
 		
 		return 0;
-		
 	}
-	
+
+	public static int diceCount(int[] dice, int num) {
+		int sum = 0;
+		for (int i = 0; i < dice.length; i++) {
+			if (dice[i] == num) {
+				sum++;
+			}
+		}
+		return sum;
+	}
 
 }
--- src/RollDiceTest.java
+++ src/RollDiceTest.java
@@ -53,6 +53,74 @@ class RollDiceTest {
 		System.out.println(RollDice.yahtzee(array, "yahtzee"));
 	}
 	
+	@Test
+	void testPair1() {
+		int[] array = {1,1,1,1,1};
+		assertEquals(2, RollDice.yahtzee(array, "pair"));
+		System.out.println(RollDice.yahtzee(array, "pair"));
+	}
 	
+	@Test
+	void testPair2() {
+		int[] array = {1,1,2,2,2};
+		assertEquals(4, RollDice.yahtzee(array, "pair"));
+		System.out.println(RollDice.yahtzee(array, "pair"));
+	}
+	
+	@Test
+	void testThreeOfAKind1() {
+		int[] array = {1,1,2,2,2};
+		assertEquals(6, RollDice.yahtzee(array, "three of a kind"));
+		System.out.println(RollDice.yahtzee(array, "three of a kind"));
+	}
+	
+	@Test
+	void testThreeOfAKind2() {
+		int[] array = {1,1,2,2,3};
+		assertEquals(0, RollDice.yahtzee(array, "three of a kind"));
+		System.out.println(RollDice.yahtzee(array, "three of a kind"));
+	}
+	
+	@Test
+	void testTwoPair1() {
+		int[] array = {1,1,2,2,3};
+		assertEquals(6, RollDice.yahtzee(array, "two pair"));
+		System.out.println(RollDice.yahtzee(array, "two pair"));
+	}
+	
+	@Test
+	void testTwoPair2() {
+		int[] array = {1,1,2,2,2};
+		assertEquals(6, RollDice.yahtzee(array, "two pair"));
+		System.out.println(RollDice.yahtzee(array, "two pair"));
+	}
+	
+	@Test
+	void testFourOfAKind1() {
+		int[] array = {4,4,4,4,5};
+		assertEquals(16, RollDice.yahtzee(array, "four of a kind"));
+		System.out.println(RollDice.yahtzee(array, "four of a kind"));
+	}
+	
+	@Test
+	void testFourOfAKind2() {
+		int[] array = {4,4,4,6,5};
+		assertEquals(0, RollDice.yahtzee(array, "four of a kind"));
+		System.out.println(RollDice.yahtzee(array, "four of a kind"));
+	}
+	
+	@Test
+	void testSmallStraight1() {
+		int[] array = {4,4,4,6,5};
+		assertEquals(0, RollDice.yahtzee(array, "small straight"));
+		System.out.println(RollDice.yahtzee(array, "small straight"));
+	}
+	
+	@Test
+	void testSmallStraight2() {
+		int[] array = {1,2,3,4,5};
+		assertEquals(15, RollDice.yahtzee(array, "small straight"));
+		System.out.println(RollDice.yahtzee(array, "small straight"));
+	}
 	
 }
